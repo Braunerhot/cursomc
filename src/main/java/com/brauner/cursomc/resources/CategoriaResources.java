856e4a476e271abe66d8641a5cc9010c07d2f2e5
Brauner Fernandes <braunerhot@hotmail.com>
2019-11-14 06:16:05 -0300

    Criando repository e service para Categoria

--- src/main/java/com/brauner/cursomc/resources/CategoriaResources.java
+++ src/main/java/com/brauner/cursomc/resources/CategoriaResources.java
@@ -1,41 +1,26 @@
 package com.brauner.cursomc.resources;
 
-import java.util.ArrayList;
-import java.util.List;
-
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.brauner.cursomc.domain.Categoria;
+import com.brauner.cursomc.services.CategoriaService;
 
 @RestController
 @RequestMapping(value="/categorias")
 public class CategoriaResources {
 	
-	@RequestMapping(method=RequestMethod.GET)
-	public List<Categoria> listar() {
-		
-		Categoria cat1 = new Categoria(1, "Informática");
-		Categoria cat2 = new Categoria(2, "Escritório");
-		
-		List<Categoria> lista = new ArrayList<>();
-		lista.add(cat1);
-		lista.add(cat2);
-		
-		return lista;
-		
-	}
+	@Autowired
+	private CategoriaService service;
 	
-	//@RequestMapping(method=RequestMethod.DELETE)
-	public String deletar() {
-		return "REST está deletando os dados!";
-	}
-	
-	//@RequestMapping(method=RequestMethod.POST)
-	public String incluir() {
-		return "REST está incluindo os dados!";
-	}
-	
+	@RequestMapping(value="/{id}", method=RequestMethod.GET)
+	public ResponseEntity<?> find(@PathVariable Integer id) {
+		Categoria obj = service.buscar(id); 
+		return ResponseEntity.ok().body(obj);
+	}	
 
 }
